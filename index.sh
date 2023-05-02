d35dc9e6c263b1a9f80d328d969c04518ce49fc3
Gabriel Bauer <gabriel.bauer@commerce-experts.com>
2023-05-02 07:59:45 +0200

    Add mount of outdoors data .tar.gz to docker run in index.sh

--- index.sh
+++ index.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build ./tools --tag=mighty-indexer
-docker run --rm -it --network mqsnet mighty-indexer:latest ./index.sh $*
+docker run --rm -v $(pwd)/outdoors_posts.tar.gz:/outdoors_posts.tar.gz:ro -it --network mqsnet mighty-indexer:latest ./index.sh $*
